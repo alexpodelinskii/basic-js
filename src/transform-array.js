c86d9842128c2fd91c70906666aecc8d58fdbe5f
alexpodelinskii <nikitik007@yandex.ru>
2021-03-22 01:13:37 +0300

    fix: some mistake
    Signed-off-by: alexpodelinskii <nikitik007@yandex.ru>

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+
+  if (!(Array.isArray(arr))) throw new Error;
+  let res = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    let el = arr[i]
+
+
+    if (el == '--double-prev') {
+      if (i > 0) {
+        res.push(res[res.length - 1]);
+      }
+    } else if (el == '--double-next') {
+      if (i < arr.length - 1) {
+        res.push(arr[i + 1]);
+      }
+    } else if (el == '--discard-next') {
+      if (i < arr.length) {
+        res.push(undefined);
+        i++;
+      }
+    } else if (el == '--discard-prev') {
+      if (i > 0) {
+        res.pop();
+      }
+    } else {
+      res.push(el);
+    }
+  }
+  let result = res.filter(el => { return (el !== undefined); })
+
+
+  return result;
+
 };
--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,13 +1,54 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(revers) {
+    if (revers === false) {
+      this.revers = true;
+    } else { this.revers = false; }
+  }
+  encrypt(str, key) {
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let res = "";
+
+    if (this.revers) {
+      str = str.split("").reverse().join("");
+    }
+
+    let j = 0;
+
+    for (let i = 0; i < str.length; i++) {
+
+      if (str[i] >= "A" && str[i] <= "Z") {
+        res += String.fromCharCode(((str.charCodeAt(i) + key.charCodeAt(j % key.length)) % 26) + 65);
+        j++;
+      } else res += str[i];
+    }
+
+    return res
+
+  }
+  decrypt(str, key) {
+    str = str.toUpperCase();
+    key = key.toUpperCase();
+    let res = "";
+
+    if (this.revers) {
+      str = str.split("").reverse().join("");
+    }
+    let j = 0;
+    for (let i = 0; i < str.length; i++) {
+
+      if (str[i] >= "A" && str[i] <= "Z") {
+
+        res += String.fromCharCode((26 + str.charCodeAt(i) - key.charCodeAt(j % key.length)) % 26 + 65);
+        j++;
+      } else res += str[i];
+    }
+
+    return res;
+
+
   }
 }
 
